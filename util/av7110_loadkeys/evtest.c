625f52e13deee8d9ddb48c8c48ba90ca008fa1fc
js <devnull@localhost>
2004-07-15 12:48:08 +0000

    Patch by Thomas Gereke: Sync list of known keys with Linux 2.6.7, plus cleanups.

--- util/av7110_loadkeys/evtest.c
+++ util/av7110_loadkeys/evtest.c
@@ -1,5 +1,5 @@
 /*
- * $Id: evtest.c,v 1.1 2004/01/17 16:59:46 js Exp $
+ * $Id: evtest.c,v 1.2 2004/07/15 12:48:08 js Exp $
  *
  *  Copyright (c) 1999-2000 Vojtech Pavlik
  *
@@ -36,52 +36,124 @@
 
 char *events[EV_MAX + 1] = { "Reset", "Key", "Relative", "Absolute", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
 NULL, NULL, NULL, "LED", "Sound", NULL, "Repeat", "ForceFeedback", NULL, "ForceFeedbackStatus"};
-char *keys[KEY_MAX + 1] = { "Reserved", "Esc", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "Minus", "Equal", "Backspace",
-"Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "LeftBrace", "RightBrace", "Enter", "LeftControl", "A", "S", "D", "F", "G",
-"H", "J", "K", "L", "Semicolon", "Apostrophe", "Grave", "LeftShift", "BackSlash", "Z", "X", "C", "V", "B", "N", "M", "Comma", "Dot",
-"Slash", "RightShift", "KPAsterisk", "LeftAlt", "Space", "CapsLock", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10",
-"NumLock", "ScrollLock", "KP7", "KP8", "KP9", "KPMinus", "KP4", "KP5", "KP6", "KPPlus", "KP1", "KP2", "KP3", "KP0", "KPDot", "103rd",
-"F13", "102nd", "F11", "F12", "F14", "F15", "F16", "F17", "F18", "F19", "F20", "KPEnter", "RightCtrl", "KPSlash", "SysRq",
-"RightAlt", "LineFeed", "Home", "Up", "PageUp", "Left", "Right", "End", "Down", "PageDown", "Insert", "Delete", "Macro", "Mute",
-"VolumeDown", "VolumeUp", "Power", "KPEqual", "KPPlusMinus", "Pause", "F21", "F22", "F23", "F24", "KPComma", "LeftMeta", "RightMeta",
-"Compose", "Stop", "Again", "Props", "Undo", "Front", "Copy", "Open", "Paste", "Find", "Cut", "Help", "Menu", "Calc", "Setup",
-"Sleep", "WakeUp", "File", "SendFile", "DeleteFile", "X-fer", "Prog1", "Prog2", "WWW", "MSDOS", "Coffee", "Direction",
-"CycleWindows", "Mail", "Bookmarks", "Computer", "Back", "Forward", "CloseCD", "EjectCD", "EjectCloseCD", "NextSong", "PlayPause",
-"PreviousSong", "StopCD", "Record", "Rewind", "Phone", "ISOKey", "Config", "HomePage", "Refresh", "Exit", "Move", "Edit", "ScrollUp",
-"ScrollDown", "KPLeftParenthesis", "KPRightParenthesis",
-"International1", "International2", "International3", "International4", "International5",
-"International6", "International7", "International8", "International9",
-"Language1", "Language2", "Language3", "Language4", "Language5", "Language6", "Language7", "Language8", "Language9",
-NULL, 
-"PlayCD", "PauseCD", "Prog3", "Prog4", "Suspend", "Close",
-NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-"Btn0", "Btn1", "Btn2", "Btn3", "Btn4", "Btn5", "Btn6", "Btn7", "Btn8", "Btn9",
-NULL, NULL,  NULL, NULL, NULL, NULL,
-"LeftBtn", "RightBtn", "MiddleBtn", "SideBtn", "ExtraBtn", "ForwardBtn", "BackBtn",
-NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-"Trigger", "ThumbBtn", "ThumbBtn2", "TopBtn", "TopBtn2", "PinkieBtn",
-"BaseBtn", "BaseBtn2", "BaseBtn3", "BaseBtn4", "BaseBtn5", "BaseBtn6",
-NULL, NULL, NULL, "BtnDead",
-"BtnA", "BtnB", "BtnC", "BtnX", "BtnY", "BtnZ", "BtnTL", "BtnTR", "BtnTL2", "BtnTR2", "BtnSelect", "BtnStart", "BtnMode",
-"BtnThumbL", "BtnThumbR", NULL,
-"ToolPen", "ToolRubber", "ToolBrush", "ToolPencil", "ToolAirbrush", "ToolFinger", "ToolMouse", "ToolLens", NULL, NULL,
-"Touch", "Stylus", "Stylus2", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 
-"Ok", "Select", "Goto", "Clear", "Power2", "Option", "Info", "Time", "Vendor",
-"Archive", "Program", "Channel", "Favorites", "EPG", "PVR", "MHP", "Language",
-"Title", "Subtitle", "Angle", "Zoom", "Mode", "Keyboard", "Screen", "PC", "TV",
-"TV2", "VCR", "VCR2", "Sat", "Sat2", "CD", "Tape", "Radio", "Tuner", "Player", 
-"Text", "DVD", "Aux", "MP3", "Audio", "Video", "Directory", "List", "Memo",
-"Calendar", "Red", "Green", "Yellow", "Blue", "ChannelUp", "ChannelDown", 
-"First", "Last", "AB", "Play", "Restart", "Slow", "Shuffle", "FastForward", 
-"Previous", "Next", "Digits", "Teen", "Twen", "Break" };
+char *keys[KEY_MAX + 1] = {
+  // 0x000
+  "Reserved", "Esc", "1", "2", "3", "4", "5", "6", 
+  "7", "8", "9", "0", "Minus", "Equal", "Backspace", "Tab",
+  // 0x010
+  "Q", "W", "E", "R", "T", "Y", "U", "I", 
+  "O", "P", "LeftBrace", "RightBrace", "Enter", "LeftControl", "A", "S",
+  // 0x020
+  "D", "F", "G", "H", "J", "K", "L", "Semicolon", 
+  "Apostrophe", "Grave", "LeftShift", "BackSlash", "Z", "X", "C", "V",
+  // 0x030
+  "B", "N", "M", "Comma", "Dot", "Slash", "RightShift", "KPAsterisk", 
+  "LeftAlt", "Space", "CapsLock", "F1", "F2", "F3", "F4", "F5",
+  // 0x040
+  "F6", "F7", "F8", "F9", "F10", "NumLock", "ScrollLock", "KP7",
+  "KP8", "KP9", "KPMinus", "KP4", "KP5", "KP6", "KPPlus", "KP1",
+  // 0x050
+  "KP2", "KP3", "KP0", "KPDot", NULL, "ZENKAKUHANKAKU", "102nd", "F11", 
+  "F12", "RO", "KATAKANA", "HIRAGANA", "HENKAN", "KATAKANAHIRAGANA", "MUHENKAN", "KPJPCOMMA",
+  // 0x060
+  "KPEnter", "RightCtrl", "KPSlash", "SysRq", "RightAlt", "LineFeed", "Home", "Up",
+  "PageUp", "Left", "Right", "End", "Down", "PageDown", "Insert", "Delete", 
+  // 0x070
+  "Macro", "Mute", "VolumeDown", "VolumeUp", "Power", "KPEqual", "KPPlusMinus", "Pause", 
+  NULL, "KPComma", "HANGUEL", "HANJA", "YEN", "LeftMeta", "RightMeta", "Compose",
+  // 0x080
+  "Stop", "Again", "Props", "Undo", "Front", "Copy", "Open", "Paste",
+  "Find", "Cut", "Help", "Menu", "Calc", "Setup", "Sleep", "WakeUp",
+  // 0x090
+  "File", "SendFile", "DeleteFile", "X-fer", "Prog1", "Prog2", "WWW", "MSDOS", 
+  "Coffee", "Direction", "CycleWindows", "Mail", "Bookmarks", "Computer", "Back", "Forward", 
+  // 0x0A0
+  "CloseCD", "EjectCD", "EjectCloseCD", "NextSong", "PlayPause", "PreviousSong", "StopCD", "Record",
+  "Rewind", "Phone", "ISOKey", "Config", "HomePage", "Refresh", "Exit", "Move", 
+  // 0x0B0
+  "Edit", "ScrollUp", "ScrollDown", "KPLeftParenthesis", "KPRightParenthesis", NULL, NULL, "F13",
+  "F14", "F15", "F16", "F17", "F18", "F19", "F20", "F21",
+  // 0x0C0
+  "F22", "F23", "F24", NULL, NULL, NULL, NULL, NULL,
+  "PlayCD", "PauseCD", "Prog3", "Prog4", NULL, "Suspend", "Close", "Play",
+  // 0x0D0
+  "FastForward", "BassBoost", "Print", "HP", "Camera", "Sound", "Question", "EMail",
+  "Chat", "Search", "Connect", "Finance", "Sport", "Shop", "AltErase", "Cancel",
+  // 0x0E0
+  "BrightnessDown", "BrightnessUp", "Media", NULL, NULL, NULL, NULL, NULL,
+  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+  // 0x0F0
+  "Unknown", NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+  // 0x100
+  "Btn0", "Btn1", "Btn2", "Btn3", "Btn4", "Btn5", "Btn6", "Btn7", 
+  "Btn8", "Btn9", NULL, NULL,  NULL, NULL, NULL, NULL,
+  // 0x110
+  "LeftBtn", "RightBtn", "MiddleBtn", "SideBtn", "ExtraBtn", "ForwardBtn", "BackBtn", "TaskBtn", 
+  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+  // 0x120
+  "Trigger", "ThumbBtn", "ThumbBtn2", "TopBtn", "TopBtn2", "PinkieBtn", "BaseBtn", "BaseBtn2", 
+  "BaseBtn3", "BaseBtn4", "BaseBtn5", "BaseBtn6", NULL, NULL, NULL, "BtnDead",
+  // 0x130
+  "BtnA", "BtnB", "BtnC", "BtnX", "BtnY", "BtnZ", "BtnTL", "BtnTR", 
+  "BtnTL2", "BtnTR2", "BtnSelect", "BtnStart", "BtnMode", "BtnThumbL", "BtnThumbR", NULL,
+  // 0x140
+  "ToolPen", "ToolRubber", "ToolBrush", "ToolPencil", "ToolAirbrush", "ToolFinger", "ToolMouse", "ToolLens",
+  NULL, NULL, "Touch", "Stylus", "Stylus2", "ToolDoubleTap", "ToolTripleTap", NULL,
+  // 0x150
+  "GearDown", "GearUp", NULL, NULL, NULL, NULL, NULL, NULL,
+  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+  // 0x160
+  "Ok", "Select", "Goto", "Clear", "Power2", "Option", "Info", "Time",
+  "Vendor", "Archive", "Program", "Channel", "Favorites", "EPG", "PVR", "MHP",
+  // 0x170
+  "Language", "Title", "Subtitle", "Angle", "Zoom", "Mode", "Keyboard", "Screen",
+  "PC", "TV", "TV2", "VCR", "VCR2", "Sat", "Sat2", "CD",
+  // 0x180
+  "Tape", "Radio", "Tuner", "Player", "Text", "DVD", "Aux", "MP3", 
+  "Audio", "Video", "Directory", "List", "Memo", "Calendar", "Red", "Green",
+  // 0x190
+  "Yellow", "Blue", "ChannelUp", "ChannelDown", "First", "Last", "AB", "Next",
+  "Restart", "Slow", "Shuffle", "Break", "Previous", "Digits", "Teen", "Twen",
+  // 0x1A0
+  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+  // 0x1B0
+  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+  // 0x1C0
+  "DelEOL", "DelEOS", "InsLine", "DelLine", NULL, NULL, NULL, NULL,
+  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+  // 0x1D0
+  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+  // 0x1E0
+  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+  // 0x1F0
+  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+};
 
 char *absval[5] = { "Value", "Min  ", "Max  ", "Fuzz ", "Flat " };
-char *relatives[REL_MAX + 1] = { "X", "Y", "Z", NULL, NULL, NULL, "HWheel", "Dial", "Wheel" };
-char *absolutes[ABS_MAX + 1] = { "X", "Y", "Z", "Rx", "Ry", "Rz", "Throttle", "Rudder", "Wheel", "Gas", "Brake",
-NULL, NULL, NULL, NULL, NULL,
-"Hat0X", "Hat0Y", "Hat1X", "Hat1Y", "Hat2X", "Hat2Y", "Hat3X", "Hat 3Y", "Pressure", "Distance", "XTilt", "YTilt"};
+char *relatives[REL_MAX + 1] = {
+  "X", "Y", "Z", NULL, NULL, NULL, "HWheel", "Dial", 
+  "Wheel", "Misc", NULL, NULL, NULL, NULL, NULL, NULL, 
+};
+char *absolutes[ABS_MAX + 1] = { 
+  // 0x00
+  "X", "Y", "Z", "Rx", "Ry", "Rz", "Throttle", "Rudder",
+  "Wheel", "Gas", "Brake", NULL, NULL, NULL, NULL, NULL,
+  // 0x10
+  "Hat0X", "Hat0Y", "Hat1X", "Hat1Y", "Hat2X", "Hat2Y", "Hat3X", "Hat 3Y", 
+  "Pressure", "Distance", "XTilt", "YTilt", "ToolWidth", NULL, NULL, NULL,
+  // 0x20
+  "Volume", NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+  "Misc", NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+  // 0x30
+  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+};
 char *leds[LED_MAX + 1] = { "NumLock", "CapsLock", "ScrollLock", "Compose", "Kana", "Sleep", "Suspend", "Mute" };
 char *repeats[REP_MAX + 1] = { "Delay", "Period" };
 char *sounds[SND_MAX + 1] = { "Bell", "Click" };
