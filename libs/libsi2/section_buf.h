ef6cf4a769c6a69924c43b9e7e177cbe9bc1a3f0
quincy <devnull@localhost>
2005-07-20 20:47:31 +0000

    Fix #ifdef name

--- libs/libsi2/section_buf.h
+++ libs/libsi2/section_buf.h
@@ -18,8 +18,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  */
 
-#ifndef SECTION_BUF_H
-#define SECTION_BUF_H 1
+#ifndef _SI_SECTION_BUF_H
+#define _SI_SECTION_BUF_H 1
 
 #include <stdint.h>
 
