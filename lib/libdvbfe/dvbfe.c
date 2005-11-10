1e65e6f1e0b96034b2d4db712d086104b817d055
quincy <devnull@localhost>
2005-11-10 00:17:36 +0000

    Initial checkin

---
+++ lib/libdvbfe/dvbfe.c
@@ -0,0 +1,208 @@
+/*
+ * libdvbfe - a DVB frontend library
+ *
+ * Copyright (C) 2005 Andrew de Quincey (adq_dvb@lidskialf.net)
+ * Copyright (C) 2005 Kenneth Aafloy (kenneth@linuxtv.org)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/param.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <errno.h>
+#include "dvbfe.h"
+
+int libdvbfe_open(int adapter, int frontend, int readonly)
+{
+	char filename[PATH_MAX+1];
+
+	sprintf(filename, "/dev/dvb/adapter%i/frontend%i", adapter, frontend);
+
+	if (readonly) {
+		return open(filename, O_RDONLY);
+	} else {
+		return open(filename, O_RDWR);
+	}
+}
+
+int libdvbfe_get_info(int fd, struct dvb_frontend_info *info)
+{
+	return ioctl(fd, FE_GET_INFO, info);
+}
+
+int libdvbfe_get_status(int fd, int statusmask, struct libdvbfe_status *result)
+{
+	int returnval = 0;
+
+	if (statusmask & LIBDVBFE_STATUS_FE) {
+		if (!ioctl(fd, FE_READ_STATUS, &result->status))
+			returnval |= LIBDVBFE_STATUS_FE;
+	}
+	if (statusmask & LIBDVBFE_STATUS_BER) {
+		if (!ioctl(fd, FE_READ_BER, &result->ber))
+			returnval |= LIBDVBFE_STATUS_BER;
+	}
+	if (statusmask & LIBDVBFE_STATUS_SIGNAL_STRENGTH) {
+		if (!ioctl(fd, FE_READ_SIGNAL_STRENGTH, &result->signal_strength))
+			returnval |= LIBDVBFE_STATUS_SIGNAL_STRENGTH;
+	}
+	if (statusmask & LIBDVBFE_STATUS_SNR) {
+		if (!ioctl(fd, FE_READ_SNR, &result->snr))
+			returnval |= LIBDVBFE_STATUS_SNR;
+	}
+	if (statusmask & LIBDVBFE_STATUS_UNCORRECTED_BLOCKS) {
+		if (!ioctl(fd, FE_READ_UNCORRECTED_BLOCKS, &result->ucblocks))
+			returnval |= LIBDVBFE_STATUS_UNCORRECTED_BLOCKS;
+	}
+
+	return returnval;
+}
+
+int libdvbfe_set_frontend(int fd, struct dvb_frontend_parameters *params)
+{
+	return ioctl(fd, FE_SET_FRONTEND, params);
+}
+
+int libdvbfe_get_frontend(int fd, struct dvb_frontend_parameters *params)
+{
+	return ioctl(fd, FE_GET_FRONTEND, params);
+}
+
+int libdvbfe_diseqc_command(int fd, char *command)
+{
+	int i = 0;
+	int waittime;
+	int status;
+	struct dvb_diseqc_master_cmd master_cmd;
+	unsigned int tmpcmd[6];
+
+	while(command[i]) {
+		/* kill whitespace */
+		if (isspace(command[i])) {
+			i++;
+			continue;
+		}
+
+		switch(command[i]) {
+			case 't':
+				if ((status = ioctl(fd, FE_SET_TONE, SEC_TONE_OFF)) != 0)
+					return status;
+				break;
+
+			case 'T':
+				if ((status = ioctl(fd, FE_SET_TONE, SEC_TONE_ON)) != 0)
+					return status;
+				break;
+
+			case '_':
+				if ((status = ioctl(fd, FE_SET_VOLTAGE, SEC_VOLTAGE_OFF)) != 0)
+					return status;
+				break;
+
+			case 'v':
+				if ((status = ioctl(fd, FE_SET_VOLTAGE, SEC_VOLTAGE_13)) != 0)
+					return status;
+				break;
+
+			case 'V':
+				if ((status = ioctl(fd, FE_SET_VOLTAGE, SEC_VOLTAGE_18)) != 0)
+					return status;
+				break;
+
+			case 'A':
+				if ((status = ioctl(fd, FE_DISEQC_SEND_BURST, SEC_MINI_A)) != 0)
+					return status;
+				break;
+
+			case 'B':
+				if ((status = ioctl(fd, FE_DISEQC_SEND_BURST, SEC_MINI_B)) != 0)
+					return status;
+				break;
+
+			case '+':
+				ioctl(fd, FE_ENABLE_HIGH_LNB_VOLTAGE, 1);
+				/* don't care if this one is not supported */
+				break;
+
+			case '-':
+				ioctl(fd, FE_ENABLE_HIGH_LNB_VOLTAGE, 0);
+				/* don't care if this one is not supported */
+				break;
+
+			case 'W':
+				waittime = atoi(command + i + 1);
+				if (waittime == 0) {
+					return -EINVAL;
+				}
+				usleep(waittime * 1000);
+				while(command[i] && !isspace(command[i]))
+					i++;
+				break;
+
+			case '[':
+				master_cmd.msg_len = sscanf(command+i+1, "%x %x %x %x %x %x",
+						tmpcmd, tmpcmd+1, tmpcmd+2, tmpcmd+3, tmpcmd+4, tmpcmd+5);
+				if (master_cmd.msg_len == 0)
+					return -EINVAL;
+				master_cmd.msg[0] = tmpcmd[0];
+				master_cmd.msg[1] = tmpcmd[1];
+				master_cmd.msg[2] = tmpcmd[2];
+				master_cmd.msg[3] = tmpcmd[3];
+				master_cmd.msg[4] = tmpcmd[4];
+				master_cmd.msg[5] = tmpcmd[5];
+
+				if ((status = ioctl(fd, FE_DISEQC_SEND_MASTER_CMD, &master_cmd)) != 0)
+					return status;
+
+				while(command[i] && (command[i] != ']'))
+					i++;
+				break;
+
+			default:
+				return -EINVAL;
+		}
+
+		i++;
+	}
+
+	return 0;
+}
+
+int libdvbfe_diseqc_read(int fd, int timeout, unsigned char *buf, unsigned int len)
+{
+	struct dvb_diseqc_slave_reply reply;
+	int result;
+
+	if (len > 4)
+		len = 4;
+
+	reply.timeout = timeout;
+	reply.msg_len = len;
+
+	if ((result = ioctl(fd, FE_DISEQC_RECV_SLAVE_REPLY, reply)) != 0)
+		return result;
+
+	if (reply.msg_len < len)
+		len = reply.msg_len;
+	memcpy(buf, reply.msg, len);
+
+	return len;
+}
---
+++ lib/libdvbfe/dvbfe.h
@@ -0,0 +1,115 @@
+/*
+ * libdvbfe - a DVB frontend library
+ *
+ * Copyright (C) 2005 Andrew de Quincey (adq_dvb@lidskialf.net)
+ * Copyright (C) 2005 Kenneth Aafloy (kenneth@linuxtv.org)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
+#ifndef LIBDVBFE_H
+#define LIBDVBFE_H 1
+
+#include <linux/dvb/frontend.h>
+#include <stdint.h>
+
+/**
+ * Mask of values used in the libdvbfe_get_status() call.
+ */
+#define LIBDVBFE_STATUS_FE                 0x01
+#define LIBDVBFE_STATUS_BER                0x02
+#define LIBDVBFE_STATUS_SIGNAL_STRENGTH    0x04
+#define LIBDVBFE_STATUS_SNR                0x08
+#define LIBDVBFE_STATUS_UNCORRECTED_BLOCKS 0x10
+
+/**
+ * Structure containing values used by the libdvbfe_get_status() call.
+ */
+struct libdvbfe_status {
+        fe_status_t status;
+	uint32_t ber;
+	uint16_t signal_strength;
+	uint16_t snr;
+	uint32_t ucblocks;
+};
+
+/**
+ * Open a DVB frontend.
+ *
+ * @param adapter DVB adapter ID.
+ * @param frontend Frontend ID of that adapter to open.
+ * @param readonly If 1, frontend will be opened in readonly mode only.
+ * @return A unix file descriptor on success, or -1 on failure.
+ */
+extern int libdvbfe_open(int adapter, int frontend, int readonly);
+
+/**
+ * Retrieve information on the frontend.
+ *
+ * @param fd FD opened with libdvbfe_open().
+ * @param info Place to put extracted information.
+ * @return 0 on success, nonzero on failure.
+ */
+extern int libdvbfe_get_info(int fd, struct dvb_frontend_info *info);
+
+/**
+ * Retrieve status of frontend.
+ *
+ * @param fd FD opened with libdvbfe_open().
+ * @param statusmask ORed bitmask of desired LIBDVBFE_STATUS_* values.
+ * @param result Where to put the retrieved results.
+ * @return ORed bitmask of LIBDVBFE_STATUS_* indicating which values were read successfully.
+ */
+extern int libdvbfe_get_status(int fd, int statusmask, struct libdvbfe_status *result);
+
+/**
+ * Set the frontend tuning parameters.
+ *
+ * @param fd FD opened with libdvbfe_open().
+ * @param params Params to set.
+ * @return 0 on success, nonzero on failure.
+ */
+extern int libdvbfe_set_frontend(int fd, struct dvb_frontend_parameters *params);
+
+/**
+ * Read the current frontend tuning parameters from the hardware.
+ *
+ * @param fd FD opened with libdvbfe_open().
+ * @param params Where to put the parameters.
+ * @return 0 on success, nonzero on failure.
+ */
+extern int libdvbfe_get_frontend(int fd, struct dvb_frontend_parameters *params);
+
+/**
+ * Execute a DISEQC command string (format as specified in libdvbcfg diseqc.conf).
+ *
+ * @param fd FD opened with libdvbfe_open().
+ * @param command Command to execute.
+ * @return 0 on success, nonzero on failure.
+ */
+extern int libdvbfe_diseqc_command(int fd, char *command);
+
+/**
+ * Read a DISEQC response from the frontend.
+ *
+ * @param fd FD opened with libdvbfe_open().
+ * @param timeout Timeout for DISEQC response.
+ * @param buf Buffer to store response in.
+ * @param len Number of bytes in buffer.
+ * @return >= 0 on success (number of received bytes), <0 on failure.
+ */
+extern int libdvbfe_diseqc_read(int fd, int timeout, unsigned char *buf, unsigned int len);
+
+#endif // LIBDVBFE_H
