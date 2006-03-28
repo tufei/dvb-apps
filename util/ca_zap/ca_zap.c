77e38363eb60fa4cd185f5b6f7ab871e0cbc0252
Andrew de Quincey <adq_dvb@lidskialf.net>
2006-03-28 15:34:10 +0100

    First checkin of ca_zap porting to libucsi. Non-functional yet

--- util/ca_zap/ca_zap.c
+++ util/ca_zap/ca_zap.c
@@ -21,30 +21,33 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <limits.h>
 #include <string.h>
-#include <linux/dvb/frontend.h>
-
-#include "si.h"
-#include "channels.h"
-#include "pmt.h"
-#include "en50221_hlci.h"
+#include <dvbcfg/dvbcfg_zapchannel.h>
+#include <dvbapi/dvbdemux.h>
+#include <dvbapi/dvbca.h>
+#include <dvben50221/en50221_app_ca.h>
+#include <ucsi/section.h>
+#include <ucsi/mpeg/section.h>
 
 #include "ca_zap.h"
+
+static struct section_ext *read_section_ext(char *buf, int buflen, int adapter, int demux, int pid, int table_id);
+static void set_cam_hlci(int cafd, char *capmt, int capmtsize);
+static void set_cam_link(int cafd, char *capmt, int capmtsize);
 
 void usage(void)
 {
 	static const char *usage = "\n"
-		" CA-ZAP: A High level CA zapping application based upon the HLCI implementation\n"
+		" CA-ZAP: A CA zapping application for HLCI or Link layer CAMs\n"
 		" Copyright (C) 2004, 2005 Manu Abraham (manu@kromtek.com)\n\n"
 		" usage: ca_zap options\n"
 		" -h	help\n"
 		" -a	adapter to use\n"
 		" -c	channels.conf file\n"
 		" -n	channel name to zap\n"
-		" -t	frontend type, options: sat/cab/ter\n"
 		" -d	demux to use\n"
 		" -s	CA module (Slot to use)\n"
-		" -f	Frontend to use\n"
 		" -m    Move CA descriptors from stream to programme level if possible\n";
 
 	fprintf(stderr, "%s\n", usage);
@@ -52,23 +55,25 @@ void usage(void)
 	exit(1);
 }
 
-
-#define MAX_PATH_LENGTH		80
-
 int main(int argc, char *argv[])
 {
-	struct service_info *p_si = (struct service_info *) malloc(sizeof (struct service_info));
-	struct channel_params *p_channel_params = (struct channel_params *) malloc(sizeof (struct channel_params));
-	struct en50221_pmt_object *p_en50221_pmt_object = p_en50221_pmt_object = (struct en50221_pmt_object *) malloc( sizeof (struct en50221_pmt_object));
+	struct dvbcfg_zapchannel *channels = NULL;
+	struct dvbcfg_zapchannel *channel;
+	char buf[4096];
+	int pmt_pid = -1;
+	struct section_ext *section_ext;
+	struct mpeg_pat_section *pat;
+	struct mpeg_pat_program *cur_program;
+	struct mpeg_pmt_section *pmt;
 
-	char chanfile[MAX_PATH_LENGTH];
+	char chanfile[PATH_MAX];
 	char channel_name[20];
-	char fe_type_name[4];
-	char adapter[80], frontend_dev[80], demux_dev[80], ca_dev[80];
-	uint8_t dev = 0, fe_type = 0, demux = 0, ca = 0, frontend = 0;
+	uint8_t dev = 0, demux = 0, ca = 0;
 	int opt;
 	int move_to_programme = 0;
 
+	chanfile[0] = 0;
+	channel_name[0] = 0;
 	if (argc < 3)
 		usage();
 	// a = adapter, c = channels.conf, t = type (S/C/T), d = demux, s = CA device, f = frontend device n = channel name
@@ -86,21 +91,6 @@ int main(int argc, char *argv[])
 				strncpy(channel_name, optarg, sizeof(channel_name));
 				break;
 
-			case 't':
-				strncpy(fe_type_name, optarg, sizeof(fe_type_name));
-				if (strncmp(fe_type_name, "sat", sizeof(fe_type_name)) == 0)
-					fe_type = 0x01;
-				else if (strncmp(fe_type_name, "cab", sizeof(fe_type_name)) == 0)
-					fe_type = 0x02;
-				else if (strncmp(fe_type_name, "ter", sizeof(fe_type_name)) == 0)
-					fe_type = 0x03;
-
-				else {
-					printf(" ERROR: Invalid Frontend type %s\n", fe_type_name);
-					exit(-1);
-				}
-				break;
-
 			case 'd':
 				demux = strtoul(optarg, NULL, 0);
 				break;
@@ -110,7 +100,7 @@ int main(int argc, char *argv[])
 				break;
 
 			case 'f':
-				frontend = strtoul(optarg, NULL, 0);
+				// deprecated
 				break;
 
 			case 'm':
@@ -125,27 +115,137 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	snprintf(adapter, sizeof(adapter), "/dev/dvb/adapter%i", dev);
-	snprintf(frontend_dev, sizeof(frontend_dev), "/dev/dvb/adapter%i/frontend%i", dev, frontend);
-	snprintf(demux_dev, sizeof(demux_dev), "/dev/dvb/adapter%i/demux%i", dev, demux);
-	snprintf(ca_dev, sizeof(ca_dev), "/dev/dvb/adapter%i/ca%i", dev, ca);
-	printf("Using Adpater=[%s]\n Frontend=[%s]\n Demux=[%s]\n Slot=[%s]\n", adapter, frontend_dev, demux_dev, ca_dev);
-
-	if (p_channel_params == NULL) {
-		printf("Could not allocate memory.\n");
-		exit(-1);
+	// parse the supplied channel list
+	if (dvbcfg_zapchannel_load(chanfile, &channels)) {
+		fprintf(stderr, "Failed to load channels %s\n", chanfile);
+		exit(1);
 	}
 
-	parse_channel_list(p_channel_params, chanfile, channel_name, fe_type);
-	parse_si(p_si, p_channel_params, adapter, frontend_dev, demux_dev);
-	if (p_en50221_pmt_object != NULL) {
-		do_en50221_pmt_object(p_en50221_pmt_object, p_si, ONLY, OK_DESCRAMBLING, move_to_programme);
-		write_en50221_pmt_object(p_en50221_pmt_object, ca_dev);
+	// find the requested channel
+	if ((channel = dvbcfg_zapchannel_find(channels, channel_name)) == NULL) {
+		fprintf(stderr, "Unable to find requested channel %s\n", channel_name);
+		exit(1);
+	}
 
-	} else {
-		printf("%s: Memory allocation failed\n", __FUNCTION__);
-		return -1;
+	// read the PAT
+	section_ext = read_section_ext(buf, sizeof(buf), dev, demux, TRANSPORT_PAT_PID, stag_mpeg_program_association);
+	if (section_ext == NULL) {
+		fprintf(stderr, "Failed to read PAT\n");
+		exit(1);
+	}
+
+	// process the PAT to find the requested program's PMT PID
+	pat = mpeg_pat_section_codec(section_ext);
+	if (pat == NULL) {
+		fprintf(stderr, "Bad PAT received\n");
+		exit(1);
+	}
+	mpeg_pat_section_programs_for_each(pat, cur_program) {
+		if (cur_program->program_number == channel->channel_number) {
+			pmt_pid = cur_program->pid;
+			break;
+		}
+	}
+	if (pmt_pid == -1) {
+		fprintf(stderr, "Did not find requested program in PAT\n");
+		exit(1);
+	}
+
+	// read the PMT
+	section_ext = read_section_ext(buf, sizeof(buf), dev, demux, pmt_pid, stag_mpeg_program_map);
+	if (section_ext == NULL) {
+		fprintf(stderr, "Failed to read PMT\n");
+		exit(1);
+	}
+	pmt = mpeg_pmt_section_codec(section_ext);
+	if (pmt == NULL) {
+		fprintf(stderr, "Bad PMT received\n");
+		exit(1);
+	}
+
+	// process the PMT into a CAPMT
+	uint8_t capmt[4096];
+	int size = en50221_ca_format_pmt(pmt, capmt, sizeof(capmt),
+					 CA_LIST_MANAGEMENT_ONLY, CA_PMT_CMD_ID_OK_DESCRAMBLING);
+	if (size < 0) {
+		fprintf(stderr, "Failed to format PMT");
+		exit(1);
+	}
+
+	// open the CA device and dispatch depending on its type
+	int cafd = dvbca_open(dev, ca);
+	if (cafd < 0) {
+		fprintf(stderr, "Failed to open CA device\n");
+		exit(1);
+	}
+	if (dvbca_get_cam_state(cafd) != DVBCA_CAMSTATE_READY) {
+		fprintf(stderr, "CAM not detected in CA slot\n");
+		exit(1);
+	}
+	switch(dvbca_get_interface_type(cafd)) {
+	case DVBCA_INTERFACE_LINK:
+		set_cam_link(cafd, capmt, size);
+		break;
+
+	case DVBCA_INTERFACE_HLCI:
+		set_cam_hlci(cafd, capmt, size);
+		break;
+
+	default:
+		fprintf(stderr, "Unknown CA interface type\n");
+		exit(1);
 	}
 
 	return 0;
+}
+
+static struct section_ext *read_section_ext(char *buf, int buflen, int adapter, int demux, int pid, int table_id)
+{
+	int demux_fd = -1;
+	char filter[18];
+	char mask[18];
+	int size;
+	struct section *section;
+	struct section_ext *result = NULL;
+
+	// open the demuxer
+	if ((demux_fd = dvbdemux_open_demux(adapter, demux, 0)) < 0) {
+		goto exit;
+	}
+
+	// create a section filter
+	memset(filter, 0, sizeof(filter));
+	memset(mask, 0, sizeof(mask));
+	filter[0] = table_id;
+	mask[0] = 0xFF;
+	if (dvbdemux_set_section_filter(demux_fd, pid, filter, mask, 1, 1)) {
+		goto exit;
+	}
+
+	// read the section
+	if ((size = read(demux_fd, buf, buflen)) < 0) {
+		goto exit;
+	}
+
+	// parse it as a section
+	section = section_codec(buf, size);
+	if (section == NULL) {
+		goto exit;
+	}
+
+	// parse it as a section_ext
+	result = section_ext_decode(section, 0);
+
+exit:
+	if (demux_fd != -1)
+		close(demux_fd);
+	return result;
+}
+
+static void set_cam_hlci(int cafd, char *capmt, int capmtsize) {
+	// FIXME
+}
+
+static void set_cam_link(int cafd, char *capmt, int capmtsize) {
+	// FIXME
 }
