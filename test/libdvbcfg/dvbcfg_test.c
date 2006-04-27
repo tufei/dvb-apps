febdad5a38dfafdcc30e4faacc307cb0cfd8f7ab
Andrew de Quincey <adq_dvb@lidskialf.net>
2006-04-27 11:53:50 +0100

    Clean up unnecessary tests

--- test/libdvbcfg/dvbcfg_test.c
+++ test/libdvbcfg/dvbcfg_test.c
@@ -20,59 +20,19 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <libdvbcfg/dvbcfg_source_backend_file.h>
-#include <libdvbcfg/dvbcfg_diseqc_backend_file.h>
-#include <libdvbcfg/dvbcfg_adapter_backend_file.h>
-#include <libdvbcfg/dvbcfg_multiplex_backend_file.h>
-#include <libdvbcfg/dvbcfg_seed_backend_file.h>
+#include <string.h>
 #include <libdvbcfg/dvbcfg_vdrchannel.h>
 #include <libdvbcfg/dvbcfg_zapchannel.h>
 
-void syntax();
-
+void syntax(void);
 
 int main(int argc, char *argv[])
 {
-        struct dvbcfg_source *sources = NULL;
-        struct dvbcfg_source_backend* source_backend;
-        struct dvbcfg_diseqc_backend* diseqc_backend;
-        struct dvbcfg_adapter_backend* adapter_backend;
-        struct dvbcfg_multiplex_backend* multiplex_backend;
-        struct dvbcfg_seed_backend* seed_backend;
-
-        if (argc != 5) {
+        if (argc != 4) {
                 syntax();
         }
 
-        if (!strcmp(argv[1], "-source")) {
-                dvbcfg_source_backend_file_create(argv[2], &source_backend);
-                dvbcfg_source_load(source_backend, &sources);
-                dvbcfg_source_backend_file_destroy(source_backend);
-
-                dvbcfg_source_backend_file_create(argv[3], &source_backend);
-                dvbcfg_source_save(source_backend, sources);
-                dvbcfg_source_backend_file_destroy(source_backend);
-                dvbcfg_source_free_all(sources);
-
-        } else if (!strcmp(argv[1], "-diseqc")) {
-                struct dvbcfg_diseqc *diseqcs = NULL;
-
-                dvbcfg_source_backend_file_create(argv[4], &source_backend);
-                dvbcfg_source_load(source_backend, &sources);
-                dvbcfg_source_backend_file_destroy(source_backend);
-
-                dvbcfg_diseqc_backend_file_create(argv[2], &sources, 1, &diseqc_backend);
-                dvbcfg_diseqc_load(diseqc_backend, &diseqcs);
-                dvbcfg_diseqc_backend_file_destroy(diseqc_backend);
-
-                dvbcfg_diseqc_backend_file_create(argv[3], &sources, 1, &diseqc_backend);
-                dvbcfg_diseqc_save(diseqc_backend, diseqcs);
-                dvbcfg_diseqc_backend_file_destroy(diseqc_backend);
-
-                dvbcfg_diseqc_free_all(diseqcs);
-                dvbcfg_source_free_all(sources);
-
-        } else if (!strcmp(argv[1], "-vdrchannel")) {
+        if (!strcmp(argv[1], "-vdrchannel")) {
                 struct dvbcfg_vdrchannel *vdrchannels = NULL;
 
                 dvbcfg_vdrchannel_load(argv[2], &vdrchannels);
@@ -85,74 +45,6 @@ int main(int argc, char *argv[])
                 dvbcfg_zapchannel_save(argv[3], zapchannels);
                 dvbcfg_zapchannel_free_all(zapchannels);
 
-        } else if (!strcmp(argv[1], "-adapter")) {
-                struct dvbcfg_adapter *adapters = NULL;
-
-                dvbcfg_source_backend_file_create(argv[4], &source_backend);
-                dvbcfg_source_load(source_backend, &sources);
-                dvbcfg_source_backend_file_destroy(source_backend);
-
-                dvbcfg_adapter_backend_file_create(argv[2], &sources, 1, &adapter_backend);
-                dvbcfg_adapter_load(adapter_backend, &adapters);
-                dvbcfg_adapter_backend_file_destroy(adapter_backend);
-
-                dvbcfg_adapter_backend_file_create(argv[3], &sources, 1, &adapter_backend);
-                dvbcfg_adapter_save(adapter_backend, adapters);
-                dvbcfg_adapter_backend_file_destroy(adapter_backend);
-
-                dvbcfg_adapter_free_all(adapters);
-                dvbcfg_source_free_all(sources);
-
-        } else if (!strcmp(argv[1], "-multiplex-long")) {
-                struct dvbcfg_multiplex *multiplexes = NULL;
-
-                dvbcfg_source_backend_file_create(argv[4], &source_backend);
-                dvbcfg_source_load(source_backend, &sources);
-                dvbcfg_source_backend_file_destroy(source_backend);
-
-                dvbcfg_multiplex_backend_file_create(argv[2], &sources, 1, 1, &multiplex_backend);
-                dvbcfg_multiplex_load(multiplex_backend, &multiplexes);
-                dvbcfg_multiplex_backend_file_destroy(multiplex_backend);
-
-                dvbcfg_multiplex_backend_file_create(argv[3], &sources, 1, 1, &multiplex_backend);
-                dvbcfg_multiplex_save(multiplex_backend, multiplexes);
-                dvbcfg_multiplex_backend_file_destroy(multiplex_backend);
-
-                dvbcfg_multiplex_free_all(multiplexes);
-                dvbcfg_source_free_all(sources);
-
-        } else if (!strcmp(argv[1], "-multiplex-short")) {
-                struct dvbcfg_multiplex *multiplexes = NULL;
-
-                dvbcfg_source_backend_file_create(argv[4], &source_backend);
-                dvbcfg_source_load(source_backend, &sources);
-                dvbcfg_source_backend_file_destroy(source_backend);
-
-                dvbcfg_multiplex_backend_file_create(argv[2], &sources, 1, 0, &multiplex_backend);
-                dvbcfg_multiplex_load(multiplex_backend, &multiplexes);
-                dvbcfg_multiplex_backend_file_destroy(multiplex_backend);
-
-                dvbcfg_multiplex_backend_file_create(argv[3], &sources, 1, 0, &multiplex_backend);
-                dvbcfg_multiplex_save(multiplex_backend, multiplexes);
-                dvbcfg_multiplex_backend_file_destroy(multiplex_backend);
-
-                dvbcfg_multiplex_free_all(multiplexes);
-                dvbcfg_source_free_all(sources);
-
-        } else if (!strcmp(argv[1], "-seed")) {
-                struct dvbcfg_seed *seeds = NULL;
-                struct dvbcfg_source *sources = NULL;
-
-                dvbcfg_seed_backend_file_create(argv[2], 1, &sources, 1, &seed_backend);
-                dvbcfg_seed_load(seed_backend, &seeds);
-                dvbcfg_seed_backend_file_destroy(seed_backend);
-
-		dvbcfg_seed_backend_file_create(argv[3], 1, &sources, 1, &seed_backend);
-                dvbcfg_seed_save(seed_backend, seeds);
-                dvbcfg_seed_backend_file_destroy(seed_backend);
-
-                dvbcfg_seed_free_all(seeds);
-
         } else {
                 syntax();
         }
@@ -163,6 +55,6 @@ int main(int argc, char *argv[])
 void syntax()
 {
         fprintf(stderr,
-                "Syntax: dvcfg_test <-source|-diseqc|-vdrchannel|-zapchannel|-adapter|-multiplex-long|-multiplex-short|-seed> <input filename> <output filename> <sources filename>\n");
+                "Syntax: dvcfg_test <-vdrchannel|-zapchannel> <input filename> <output filename>\n");
         exit(1);
 }
