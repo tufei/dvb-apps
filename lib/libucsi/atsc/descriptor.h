7d263c8a621ac72a93bc721c8b0da59dc93f77d0
Andrew de Quincey <adq_dvb@lidskialf.net>
2006-05-19 15:30:37 +0100

    Clean up ATSC descriptor/section tags Add support for PSIP sections.

--- lib/libucsi/atsc/descriptor.h
+++ lib/libucsi/atsc/descriptor.h
@@ -33,34 +33,20 @@ extern "C"
  * Enumeration of ATSC descriptor tags.
  */
 enum atsc_descriptor_tag {
-
-/* A 52/A describes a
- * dtag_atsc_ac3_registration			= 0x05, */
-
-/* A90 describes a
- * dtag_atsc_association_tag			= 0x14, */
-
 	dtag_atsc_stuffing			= 0x80,
 	dtag_atsc_ac3_audio			= 0x81,
 	dtag_atsc_caption_service		= 0x86,
 	dtag_atsc_content_advisory		= 0x87,
-	dtag_atsc_ca				= 0x88,
 	dtag_atsc_extended_channel_name		= 0xa0,
 	dtag_atsc_service_location		= 0xa1,
 	dtag_atsc_time_shifted_service		= 0xa2,
 	dtag_atsc_component_name		= 0xa3,
-	dtag_atsc_data_service			= 0xa4,
-	dtag_atsc_pid_count			= 0xa5,
-	dtag_atsc_download			= 0xa6,
-	dtag_atsc_MPE				= 0xa7,
 	dtag_atsc_dcc_departing_request		= 0xa8,
 	dtag_atsc_dcc_arriving_request		= 0xa9,
 	dtag_atsc_redistribution_control	= 0xaa,
-	dtag_atsc_dcc_location_code		= 0xab,
 	dtag_atsc_private_information		= 0xad,
-	dtag_atsc_module_link			= 0xb4,
-	dtag_atsc_crc32				= 0xb5,
-	dtag_atsc_group_link			= 0xb8,
+	dtag_atsc_content_identifier		= 0xb6,
+	dtag_atsc_genre				= 0xab,
 };
 
 #ifdef __cplusplus
--- lib/libucsi/atsc/section.h
+++ lib/libucsi/atsc/section.h
@@ -19,6 +19,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  */
 
+#include <libucsi/section.h>
+
 #ifndef _UCSI_ATSC_SECTION_H
 #define _UCSI_ATSC_SECTION_H 1
 
@@ -31,36 +33,40 @@ extern "C"
  * Enumeration of ATSC section tags.
  */
 enum atsc_section_tag {
-/* 0xC0-0xC6   [ATSC coordinated values which are defined in other standards.] */
-	stag_atsc_master_guide				= 0xc7,
+	stag_atsc_master_guide					= 0xc7,
 	stag_atsc_terrestrial_virtual_channel			= 0xc8,
-	stag_atsc_cable_virtual_channel			= 0xc9,
-	stag_atsc_rating_region				= 0xca,
+	stag_atsc_cable_virtual_channel				= 0xc9,
+	stag_atsc_rating_region					= 0xca,
 	stag_atsc_event_informationen				= 0xcb,
-	stag_atsc_extended_text				= 0xcc,
+	stag_atsc_extended_text					= 0xcc,
 	stag_atsc_system_time					= 0xcd,
-
-	stag_atsc_data_event					= 0xce,
-	stag_atsc_data_service				= 0xcf,
-	stag_atsc_network_resources				= 0xd1,
-	stag_atsc_long_term_serivce				= 0xd2,
-/* identical to DVB/ISO ?
-	0x3F        DSM-CC Addressable Section Table
-	0x3B        DSM-CC Section Table
-	0x3C        DSM-CC Section Table */
-
-	/* 0xCE-0xD2   [ATSC coordinated values which are defined in other standards.] */
 	stag_atsc_directed_channel_change			= 0xd3,
 	stag_atsc_directed_channel_change_selection_code	= 0xd4,
-
-	/* 0xD5-0xDF   [ATSC coordinated values which are defined in other standards.] */
-	stag_atsc_aggregate_event_information			= 0xd6,
-	stag_atsc_aggregate_extended_text			= 0xd7,
-	stag_atsc_satellite_virtual_channel			= 0xda,
-
-	/* 0xE0-0xE5   [Used in other systems] */
-	/* 0xE6-0xFE   [Reserved for future ATSC use] */
 };
+
+/**
+ * ATSC specific PSIP section structure.
+ */
+struct section_psip {
+	struct section_ext		section_ext;
+	uint8_t				protocol_version;
+} __ucsi_packed;
+
+/**
+ * Decode a PSIP section structure.
+ *
+ * @param section_ext Pointer to the processed section_ext structure.
+ * @return Pointer to the parsed section_psip structure, or NULL if invalid.
+ */
+static inline struct section_psip *section_psip_decode(struct section_ext *section_ext)
+{
+	size_t len = section_ext_length(section_ext);
+	if (len < sizeof(struct section_psip)) {
+		return NULL;
+	}
+
+	return (struct section_psip *) section_ext;
+}
 
 #ifdef __cplusplus
 }
