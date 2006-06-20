91cb35116fe36975b6e6f9c114a9ba78be065513
Andrew de Quincey <adq_dvb@lidskialf.net>
2006-06-20 15:04:58 +0100

    Add extended channel name descriptor Add service location descriptor

--- lib/libucsi/atsc/descriptor.h
+++ lib/libucsi/atsc/descriptor.h
@@ -30,6 +30,8 @@ extern "C"
 #include <libucsi/endianops.h>
 #include <libucsi/atsc/caption_service_descriptor.h>
 #include <libucsi/atsc/content_advisory_descriptor.h>
+#include <libucsi/atsc/extended_channel_name_descriptor.h>
+#include <libucsi/atsc/service_location_descriptor.h>
 
 /**
  * Enumeration of ATSC descriptor tags.
