871edad9b6ad0c4f634dae7afa9ac61469748e2d
Andrew de Quincey <adq_dvb@lidskialf.net>
2006-11-20 00:04:08 +0000

    Skeleton scan loop

--- util/dvbscan/dvbscan.c
+++ util/dvbscan/dvbscan.c
@@ -19,7 +19,17 @@
 	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
+#include <stdlib.h>
 #include "dvbscan.h"
+
+// transponders we have yet to scan
+static struct transponder *toscan = NULL;
+static struct transponder *toscan_end = NULL;
+
+// transponders we have scanned
+static struct transponder *scanned = NULL;
+static struct transponder *scanned_end = NULL;
+
 
 int main(int argc, char *argv[])
 {
@@ -27,7 +37,30 @@ int main(int argc, char *argv[])
 
 	// FIXME: load the initial scan file
 
-	// FIXME: scan loop!
+	// main scan loop
+	while(toscan) {
+		// FIXME: have we already scanned this one?
+
+		// FIXME: tune it
+
+		// FIXME: scan it
+
+		// add to scanned list.
+		if (scanned_end == NULL) {
+			scanned = toscan;
+		} else {
+			scanned_end->next = toscan;
+		}
+		scanned_end = toscan;
+		toscan->next = NULL;
+
+		// remove from toscan list
+		toscan = toscan->next;
+		if (toscan == NULL)
+			toscan_end = NULL;
+	}
+
+	// FIXME: output the data
 
 	return 0;
 }
