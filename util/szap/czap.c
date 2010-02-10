38fd16097e021c3539aa555a595b192778720287
Manu Abraham <abraham.manu@gmail.com>
2010-02-10 22:58:12 +0400

    czap: reformat and extend usage string
    From: Janne Grunau <j@jannau.net>

--- util/szap/czap.c
+++ util/szap/czap.c
@@ -241,9 +241,19 @@ int check_frontend (int fe_fd, int human_readable)
 }
 
 
-static const char *usage = "\nusage: %s [-a adapter_num] [-f frontend_id] [-d demux_id] [-c conf_file] [ -H ] {<channel name>| -n channel_num} [-x]\n"
-	"   or: %s [-c conf_file]  -l\n\n";
-
+static const char *usage =
+    "\nusage: %s [options]  -l\n"
+    "         list known channels\n"
+    "       %s [options] {-n channel-number|channel_name}\n"
+    "         zap to channel via number or full name (case insensitive)\n"
+    "     -a number : use given adapter (default 0)\n"
+    "     -f number : use given frontend (default 0)\n"
+    "     -d number : use given demux (default 0)\n"
+    "     -c file   : read channels list from 'file'\n"
+    "     -x        : exit after tuning\n"
+    "     -H        : human readable output\n"
+    "     -r        : set up /dev/dvb/adapterX/dvr0 for TS recording\n"
+;
 
 int main(int argc, char **argv)
 {
